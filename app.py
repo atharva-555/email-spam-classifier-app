f19f555263aaaae625ee23abeac9a46baa6f844e
atharva-555 <atharva.bobade555@gmail.com>
2024-09-08 12:20:29 +0530

    update

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
 
+# Ensure NLTK data is downloaded
+nltk.download('punkt', quiet=True)
+nltk.download('stopwords', quiet=True)
+
 # Initialize the PorterStemmer
 ps = PorterStemmer()
 
